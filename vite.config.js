bedfc978980d4c921fe4ad207c96d436c5b30401
Christophe Forycki <christophe@ondorse.co>
2024-12-19 16:21:06 +0100

    update build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import react from '@vitejs/plugin-react';
-import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        port: 8080
-    },
-    build: {
-        plugins: [react()]
-    },
-    esbuild: {
-        jsx: "automatic"
-    },
-    appType: 'spa',
-    base: '/',
-})
+  server: {
+    port: 8080,
+  },
+  build: {
+    plugins: [react()],
+  },
+  esbuild: {
+    jsx: "automatic",
+  },
+  appType: "spa",
+  base: "/",
+});
