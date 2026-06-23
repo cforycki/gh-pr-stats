508a6bffd2d925b7c75f1906e4bf626c9b7e0724
Christophe Forycki <christophe@ondorse.co>
2026-06-23 14:16:37 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@ set -e
 
 yarn build
 
-WORKTREE=$(mktemp -d)
-git worktree add "$WORKTREE" gh-pages
+git worktree prune
 
-rsync -a --delete dist/ "$WORKTREE"/
+WORKTREE=$(mktemp -d)
+trap "git worktree remove --force '$WORKTREE' 2>/dev/null || true" EXIT
+
+git worktree add "$WORKTREE" gh-pages
+rsync -a --delete --exclude='.git' dist/ "$WORKTREE"/
 
 git -C "$WORKTREE" add -A
 git -C "$WORKTREE" commit -m "deploy $(date '+%Y-%m-%d %H:%M')"
 git -C "$WORKTREE" push origin gh-pages
-
-git worktree remove "$WORKTREE"
